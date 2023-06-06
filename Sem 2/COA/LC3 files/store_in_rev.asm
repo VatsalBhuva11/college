d2c04c9ab139b7495d2f6334a977e293a394d48a
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2023-06-06 19:53:10 +0530

    subject files added

---
+++ Sem 2/COA/LC3 files/store_in_rev.asm
@@ -0,0 +1,40 @@
+.ORIG x3000
+
+
+AND R2, R2, #0;
+
+ADD R2, R2, #5;
+
+LD R1, ASCII_ZERO    ; Load the ASCII value of '0' into R1
+
+
+
+loop
+
+AND R0, R0, #0;
+ADD R0, R2, R1 ; Calculate the ASCII value of the current number
+OUT         ; Print the current number
+
+LD R0, SPACE
+
+OUT
+
+
+
+ADD R2, R2, #-1    ; Decrement n
+
+BRp loop 
+
+
+AND R2, R2, #0;
+
+ADD R2, R0, R1; Calculate the ASCII value of the current number
+
+OUT
+HALT
+
+ASCII_ZERO  .FILL x0030 ; ASCII value of '0'
+
+SPACE .FILL #32
+
+.END
